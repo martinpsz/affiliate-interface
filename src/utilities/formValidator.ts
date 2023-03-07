e6e14a40efa616ac009bd9157e26b719d499bc04
Martin Pszczola <mpszczola@afscme.com>
2023-03-07 09:31:09 -0500

    form validation utility added

--- src/utilities/formValidator.ts
+++ src/utilities/formValidator.ts
@@ -4,10 +4,15 @@ import { Reporter } from "../interfaces/interfaces";
 export const formValidator = (data: Reporter, formSection: string) => {
     let errors = {nameWarning: '', emailWarning: '', phoneWarning: ''}
     if(formSection === 'reporter-section' && data){
+
         //checks for blank fields:
         errors.nameWarning = data.name?.length === 0 ? 'This is a required field': ''
         errors.emailWarning = data.email?.length === 0 ? 'This is a required field': ''
         errors.phoneWarning = data.phone?.length ===  0 ? 'This is a required field': ''
+
+        //check for adequate number of phone digits:
+        const dateRegex = new RegExp('/\d{9}/', 'g')
+        errors.phoneWarning = data.phone?.replace(/\D+/, '').match(dateRegex) ? '' : 'Please enter a valid phone'
     }
 
     return errors
